217bf13d4cb08d565bf49ed2d67944785cc1894d
joseph townshend <joetownshend@mac.com>
2019-10-03 17:56:41 +0100

    feat: render h1 to page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+    render() {
     return (
       <div className="App">
-        <h1>I'm a React app</h1>
+        <h1>Hi, I'm a React app</h1>
+        <p>This is really working!</p>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello how are you?'));
   }
 }
 
